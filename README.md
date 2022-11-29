717a418701174dc12d851181957c00d36f4f8a3c
Kouji Sano <KoujiSano@users.noreply.github.com>
2022-11-29 05:18:14 +0000

    readme

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+kkokkokoko
+
 # Nuxt 3 Minimal Starter
 
 Look at the [nuxt 3 documentation](https://v3.nuxtjs.org) to learn more.
--- pages/grid.vue
+++ pages/grid.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <div class="row">
+    <div class="row"> 
       <div class="col">
         Column
       </div>
