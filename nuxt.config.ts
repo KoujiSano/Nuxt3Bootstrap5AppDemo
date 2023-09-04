8a83d3b8f4467f34ec9abb900ea1cfe99801a59f
Kouji Sano <KoujiSano@users.noreply.github.com>
2023-09-04 03:27:07 +0000

    update bootstrap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,16 +7,20 @@ export default defineNuxtConfig({
                 { name: 'viewport', content: 'width=device-width, initial-scale=1' }
             ],
             script: [
-                { src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js' }
+                // { src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js' }
             ],
             link: [
-                { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css' },
-                { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css' }
+                // { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css' },
+                // { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css' }
             ]
         }
     },
     modules: [
         '@vueuse/nuxt',
-        '@nuxt/content'
-      ],
+        '@nuxt/content',
+        'usebootstrap'
+    ],
+    css: [
+        "bootstrap/scss/bootstrap.scss"
+    ]
 })
